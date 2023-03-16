e4da6d381e1404b046d1d2ce5476375710df5bd6
rahul177 <rahul.sagar187@gmail.com>
2023-03-16 14:12:26 +0530

    EDA and Feature Engineering

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error, error_details:sys):
     _,_,exc_tb=error_details.exc_info()
@@ -17,10 +17,12 @@ class CustomException(Exception):
         return self.error_message
 
 """
+
 if __name__ == "__main__":
     try:
         a=1/0
     except Exception as e:
         #logging.info("Divide by Zero")
         raise CustomException(e, sys)
+
 """
